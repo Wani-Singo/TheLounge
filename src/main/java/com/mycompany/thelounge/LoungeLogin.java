e5d3dff44b1e029b96a84f00f675699019fbb7a8
Student <Student@LabVM2049939.n3oyjmpzdwxetnkof32ull2nce.jnbx.internal.cloudapp.net>
2026-09-18 09:05:41 +0200

    I added the relevant variable and the constraints that they must follow. These include the username requiring an underscore and the password being at least 8 characters long.

--- src/main/java/com/mycompany/thelounge/LoungeLogin.java
+++ src/main/java/com/mycompany/thelounge/LoungeLogin.java
@@ -9,5 +9,30 @@ package com.mycompany.thelounge;
  * @author Student
  */
 public class LoungeLogin {
+    public String firstName;
+    public String lastName;
+    public String userName;
+    public String Password;  
+    public String cellNumber;
     
+    public LoungeLogin(String firstName, String lastName, String userName, String Password, String cellNumber){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.Password = Password;
+        this.cellNumber = cellNumber; 
+    }
+    public boolean checkuserName(){
+        return userName.contains("_")&& userName.length() <= 5;
+    }
+    public boolean checkPassword(){
+        if(Password.length() <8){
+            return false;
+        }
+        boolean hasCapital = Password.matches(".*[A-Z].*");
+        boolean hasNumber = Password.matches(".*[0-9].*");
+        boolean hasSpecialCharacter = Password.matches(".*[^a-zA-Z0-9].*");
+        
+        return hasCapital && hasNumber && hasSpecialCharacter;
+    }
 }
