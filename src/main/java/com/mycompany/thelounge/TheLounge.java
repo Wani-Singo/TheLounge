cfc097e246da00429b17bd90b12df26c83a0ce70
Student <Student@LabVM2049939.n3oyjmpzdwxetnkof32ull2nce.jnbx.internal.cloudapp.net>
2026-09-18 11:52:39 +0200

    Ensured a usable main class program. This included adding a scanner to allow the add their information and a method calling the login class to do the necessary verifications.

--- src/main/java/com/mycompany/thelounge/TheLounge.java
+++ src/main/java/com/mycompany/thelounge/TheLounge.java
@@ -3,7 +3,7 @@
  */
 
 package com.mycompany.thelounge;
-
+import java.util.Scanner;
 /**
  *
  * @author Student
@@ -11,6 +11,35 @@ package com.mycompany.thelounge;
 public class TheLounge {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       LoungeLogin obj = new LoungeLogin(firstName, lastName, userName, Password, cellNumber);
+       Scanner input = new Scanner(System.in);
+       
+       System.out.println("----Welcome to The Lounge----");
+       System.out.println("--Registration");
+       
+       System.out.print("Enter you First Name");
+       String name = input.nextLine();
+       System.out.print("Enter your Last Name");
+       String lastname = input.nextLine();
+      
+       boolean isRegistered = false;
+       while(!isRegistered){
+           System.out.print("Enter Username: ");
+           String userName = input.nextLine();
+           System.out.print("Enter Password: ");
+           String Password = input.nextLine();
+           System.out.print("Enter Cellphone Number");
+           String cellNumber = input.nextLine();
+           
+           String regMessage= obj.registerUser(userName, Password, cellNumber);
+           System.out.println(regMessage);
+           
+           if(regMessage.equals("Username and password successfully captured")){
+               isRegistered = true;
+           }else{
+               System.out.println("Please try registering again with the correct details");
+           }
+       }
+      
     }
 }
